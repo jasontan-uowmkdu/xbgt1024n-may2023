ebf6f2f4cbbf46663d92b2b586afa34d1603fb09
jasontan-uowmkdu <130472328+jasontan-uowmkdu@users.noreply.github.com>
2023-06-08 20:35:16 +0800

    Add files via upload

---
+++ lecture1-activity1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+//Recap Activity
+//Category 1 - Function with no arguments and no return values
+void function_name () //since the function returns no value - void keyword is used
+{
+
+}
+
+//Category 2 - Function with arguments and no return values
+void function_name(int a, string b) // a and b are arguments/parameters passed
+{
+
+}
+
+//Category 3 - Function with arguments and one return value
+int function_name(int a, double b, string c) //function is assumed to return an int value - int keyword is used
+{
+	int some_integer; 
+
+	return some_integer;
+}
+
+//Category 4 - Function with no arguments but returns a value
+int function_name1()
+{
+	int some_integer;
+
+	return some_integer;
+}
